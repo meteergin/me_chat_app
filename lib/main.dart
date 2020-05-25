623da70c6161697b2fd88adb047b4f9d341346dd
Mete Ergin <mete.ergin@outlook.com>
2020-05-25 11:20:28 +0300

    Code review.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mechat/widgets/messageListWidget.dart';
-import 'package:mechat/widgets/messageWidget.dart';
 
 void main() {
   runApp(MyApp());
@@ -25,7 +24,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _messageText = '';
+  TextEditingController _textEditingController = new TextEditingController();
+  List<MessageListWidget> _messageListWidgetList = <MessageListWidget>[];
+
+  void _sendMessage(String text) {
+    _textEditingController.clear();
+    MessageListWidget messageListWidget = new MessageListWidget(text);
+    setState(() {
+      _messageListWidgetList.add(messageListWidget);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +43,48 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          Container(
-            child: MessageListWidget(_messageText),
+          Flexible(
+            child: ListView.builder(
+              itemBuilder: (_, int index) => _messageListWidgetList[index],
+              itemCount: _messageListWidgetList.length,
+              reverse: false,
+            ),
           ),
           SizedBox(
-            height: 100.0,
-            child: MessageWidget()
-          ),
+              height: 100.0,
+              child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: TextField(
+                          keyboardType: TextInputType.multiline,
+                          maxLines: null,
+                          decoration:
+                              InputDecoration.collapsed(hintText: "Write something ..."),
+                          controller: _textEditingController,
+                          onSubmitted: _sendMessage,
+                        )),
+                        SizedBox(
+                          width: 50.0,
+                          child: IconButton(
+                            icon: Icon(Icons.send),
+                            tooltip: 'Send message.',
+                            onPressed: () =>
+                                _sendMessage(_textEditingController.text),
+                          ),
+                        )
+                      ],
+                    ),
+                  ))),
         ],
       ),
     ));
